3a8c6fd8d1f42a9f907ec3bfce1dff4f8a22dd0f
Justin Green <justin98366@gmail.com>
2017-05-24 14:23:24 -0700

    making a correction for resubmit

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -18,10 +18,11 @@ var app = app || {};
         Authorization: githubToken
       }
     })
-      .then(
-        (data) => data.forEach(repo => repos.all.push(repo))
-      )
-    callback();
+    .then(data =>
+      {repos.all = data;
+      callback();
+
+    })
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
